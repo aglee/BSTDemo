9a5f5f542cf155d98fe1afe4d27370c32ebda317
Andy Lee <aglee@mac.com>
2018-10-05 09:34:39 -0400

    Use 1, 2, etc. as the node values, not 100, 200, etc.

--- BSTDemo/MainWindowController.m
+++ BSTDemo/MainWindowController.m
@@ -21,7 +21,7 @@
 #pragma mark - Action methods
 
 - (IBAction)resetNodes:(id)sender {
-	[self.mainView resetWithValues:@[ @100, @200, @300, @400, @500, @600, @700, @800, @900, @1000, @1100, @1200 ]];
+	[self.mainView resetWithValues:@[ @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12 ]];
 }
 
 #pragma mark - <NSWindowDelegate> methods
